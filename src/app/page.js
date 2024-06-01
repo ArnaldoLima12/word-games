a30ed58523b1fc97601e7fa7e0508c406330f91f
ArnaldoLima12 <arnaldolima588@gmail.com>
2024-06-01 12:19:47 -0300

    v1.2

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,38 @@
-import Container from '@/components/Container'
-import Header from '@/components/Header'
+import Container from '@/components/Container';
+import Header from '@/components/Header';
 import ListGame from '@/components/ListGame';
 
 export default async function Home() {
+  async function loadGame() {
+    try {
+      let res = await fetch('https://word-games-seven.vercel.app/api/list/all', {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" }
+      });
 
-    async function loadGame() {
-      try {
-
-          let res = await fetch('https://word-games-seven.vercel.app/api/list/all', {
-              method: 'GET',
-              headers: { "Content-Type": "application/json" }
-          });
-  
-          if (!res.ok) {
-              console.log('Failed to fetch data');
-              return null;
-          }
-  
-          let result = await res.json();
-          return result;
-      } 
-      catch (error) 
-      {
-          console.error('Error fetching data:', error);
-          return null; // ou lance um novo erro se preferir
+      if (!res.ok) {
+        console.log('Failed to fetch data');
+        return null;
       }
+
+      let result = await res.json();
+      return result;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return null;
+    }
   }
-  
+
   const data = await loadGame();
 
   return (
     <Container>
-      <Header auth={false}/>
-      {data && (<ListGame games={data.games}></ListGame>)}
+      <Header auth={false} />
+      {data ? (
+        <ListGame games={data.games} />
+      ) : (
+        <p>No games available.</p>
+      )}
     </Container>
-  )
+  );
 }
